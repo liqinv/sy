73116c44ed041c2fb1172c00509a90fc585a7d09
leo <55057803@qq.com>
2018-11-16 15:09:00 +0800

    关闭rabbit

--- src/main/java/com/yf/base/common/config/RabbitConfig.java
+++ src/main/java/com/yf/base/common/config/RabbitConfig.java
@@ -5,9 +5,8 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class RabbitConfig {
 
     public final static String QUEUE_NAME = "spring-boot-queue";
