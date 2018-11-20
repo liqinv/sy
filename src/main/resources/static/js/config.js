a45098456a4c3fef05151ae5da23da116e841672
leo <55057803@qq.com>
2018-11-20 15:24:40 +0800

    优化

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -2,7 +2,7 @@ var CONFIG = {
     NETWORK_ERROR:"网络错误",
     //地图默认中心点坐标
     BAIDU_LOCATION_X:104.048219,
-    BAIDU_LOCATION_Y:30.578144,
+    BAIDU_LOCATION_Y:30.583144,
     //地图默认显示层级
     BAIDU_DISPLAY_LEVEL:15,
 
